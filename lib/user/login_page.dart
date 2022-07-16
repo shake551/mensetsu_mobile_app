87d57882eca33242c39d406117486c1b3673fb85
shake <hiroki.yamako@ezweb.ne.jp>
2022-07-16 14:36:35 +0900

    [fix]fix conflict

--- lib/user/login_page.dart
+++ lib/user/login_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mensetsu_mobile_app/api/auth.dart';
 import 'package:mensetsu_mobile_app/interview/home.dart';
-import 'package:mensetsu_mobile_app/test.dart';
 import 'package:mensetsu_mobile_app/user/login_model.dart';
 import 'package:provider/provider.dart';
 
@@ -44,7 +43,6 @@ class LoginApp extends StatelessWidget {
                   validator: context.read<LoginModel>().emptyValidator,
                   onSaved: (value) =>
                       context.read<LoginModel>().username = value!,
-                  onSaved: (value) => context.read<LoginModel>().username = value!,
                 ),
                 TextFormField(
                   obscureText: !context.watch<LoginModel>().showPassword,
@@ -94,48 +92,6 @@ class LoginApp extends StatelessWidget {
                               context,
                               MaterialPageRoute(
                                 builder: (context) => InterviewHome(),
-                              ),
-                            );
-
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(
-                                content: Text('ログインしました'),
-                              ),
-                            );
-                          } else {
-                            context
-                                .read<LoginModel>()
-                                .setMessage('ユーザー名またはパスワードが誤っています');
-                          }
-                        }
-                      },
-                      child: const Text('ログイン'),
-                    ),
-                  ),
-                ),
-                Container(
-                  width: double.infinity,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 16),
-                    child: ElevatedButton(
-                      onPressed: () async {
-                        context.read<LoginModel>().setMessage('');
-
-                        if (_formKey.currentState!.validate()) {
-                          _formKey.currentState!.save();
-
-                          var response =
-                              await context.read<LoginModel>()
-                                  .auth(
-                                  context.read<LoginModel>().username,
-                                  context.read<LoginModel>().password,
-                              );
-
-                          if (response.statusCode == 200) {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => TestPage(),
                               ),
                             );
 
